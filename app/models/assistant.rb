a3a6cdebb4241b5a8ea41282a6796897876e3e1f
PabloB07 <pablob0798@gmail.com>
2021-04-22 03:12:49 -0400

    Uploading code

--- app/models/assistant.rb
+++
@@ -1,6 +0,0 @@
-class Assistant < ApplicationRecord
-  belongs_to :user
-  belongs_to :movie
-
-  accepts_nested_attributes_for :user
-end
--- test/controllers/assistants_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class AssistantsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
